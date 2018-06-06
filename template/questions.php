5212fc3baa4ec13e40ba40b39ebe719b0cfdbfc4
malubimcev <malubimcev@gmail.com>
2018-06-06 07:06:57 +0300

    v 0.4

--- template/questions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    include 'template/header.html';
-    include 'template/categories.html';
-    include 'template/questions.html';
-    include 'template/footer.html';
--- template/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include 'template/header.html';
-    include 'template/login.html';
-    include 'template/userList.html';//===================
-    include 'template/footer.html';
-
